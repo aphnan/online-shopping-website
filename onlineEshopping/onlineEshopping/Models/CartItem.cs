3cda9d8ebca136d470a07e55aa9530e85fb65f3e
aphnan <95551889+aphnan@users.noreply.github.com>
2021-12-05 03:34:36 +0300

    Asp.net

---
+++ onlineEshopping/onlineEshopping/Models/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WingtipToys.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public System.DateTime DateCreated { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
---
+++ onlineEshopping/onlineEshopping/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
